60862ec3927eb959183d10a25fb62b593dcb000f
ohmy-cz <jcy@siteimprove.com>
2020-03-23 13:59:07 +0100

    Improving readability

--- net.jancerveny.sofaking.Common/Enums/MovieStatusEnum.cs
+++ net.jancerveny.sofaking.Common/Enums/MovieStatusEnum.cs
@@ -1,22 +1,22 @@
 ﻿public enum MovieStatusEnum
 {
-    WatchingFor,
-    DownloadQueued,
-    Downloading,
-    DownloadingPaused,
-    Downloaded,
-    NoVideoFilesError,
-    TranscodingQueued,
-    AnalysisStarted,
-    TranscodingStarted,
-    TranscodingFinished,
-    TranscodingError,
-    Finished,
-    FileInUse,
-    FileNotFound,
-    CouldNotCleanup,
-    TranscodingIncomplete,
-    TranscodingRunningTooLong, // Transcoding took more than 24 hours
-    AnalysisAudioFailed,
-    AnalysisVideoFailed
+    WatchingFor = 0,
+    DownloadQueued = 1,
+    Downloading = 2,
+    DownloadingPaused = 3,
+    Downloaded = 4,
+    NoVideoFilesError = 5,
+    TranscodingQueued = 6,
+    AnalysisStarted = 7,
+    TranscodingStarted = 8,
+    TranscodingFinished = 9,
+    TranscodingError = 10,
+    Finished = 11,
+    FileInUse = 12,
+    FileNotFound = 13,
+    CouldNotCleanup = 14,
+    TranscodingIncomplete = 15,
+    TranscodingRunningTooLong = 16, // Transcoding took more than 24 hours
+    AnalysisAudioFailed = 17,
+    AnalysisVideoFailed = 18
 }
